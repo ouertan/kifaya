7c606e52aed6a274c8694eb3844130c409be393e
heeba O <161805168+ouertan@users.noreply.github.com>
2026-09-18 20:02:24 +0100

    Add files via upload

---
+++ client/src/App.tsx
@@ -0,0 +1,30 @@
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/NotFound";
+import Landing from "@/pages/Landing";
+import Login from "@/pages/Login";
+import Home from "@/pages/Home";
+import { Route, Switch } from "wouter";
+import ErrorBoundary from "./components/ErrorBoundary";
+import { ThemeProvider } from "./contexts/ThemeContext";
+
+function App() {
+  return (
+    <ErrorBoundary>
+      <ThemeProvider defaultTheme="light">
+        <TooltipProvider>
+          <Toaster />
+          <Switch>
+            <Route path="/" component={Landing} />
+            <Route path="/login" component={Login} />
+            <Route path="/app" component={Home} />
+            <Route path="/workspace" component={Home} />
+            <Route path="/404" component={NotFound} />
+            <Route component={NotFound} />
+          </Switch>
+        </TooltipProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
+  );
+}
+export default App;
